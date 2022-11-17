4bbbf6641fe48af9622d2182793f64589960ee0c
Giuseppe Romagnuolo <giuseppe.romagnuolo@allenovery.com>
2022-11-17 12:28:08 +0000

    Triggering Grid Search

--- src/hyperparam_tuning/grid_search.py
+++ src/hyperparam_tuning/grid_search.py
@@ -2,7 +2,7 @@ import itertools
 import subprocess
 
 
-max_ngram_values = [11, 12]
+max_ngram_values = [13, 14]
 max_itr_values = [4, 8]
 
 for max_ngram, max_itr in itertools.product(max_ngram_values, max_itr_values):
